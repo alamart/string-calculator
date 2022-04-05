00678b0df75673ac8765412168e579b7b3699793
Alvin LAMARTINIERE <alvin.lamartiniere@hubvisory.com>
2022-04-05 18:48:55 +0200

    [Step4]Feat:Handle new lines between numbers

--- stringcalculator/src/main/java/org/kata/stringcalculator/StringCalculator.java
+++ stringcalculator/src/main/java/org/kata/stringcalculator/StringCalculator.java
@@ -3,12 +3,15 @@ package org.kata.stringcalculator;
 import java.util.List;
 
 public class StringCalculator {
+	
+	private static String SEPARATOR = ",";
+	private static String NEWLINE_SEPARATOR = "\\n";
 
 	public static int add(String numbers) {
 		if(isZeroSum(numbers)) {
 			return 0;
 		}
-		List<String> numbersList = List.of(numbers.split(","));
+		List<String> numbersList = splitNumbers(numbers);
 		return numbersList.stream().map(Integer::valueOf).reduce(0, (acc, current) -> acc+current);
 	}
 	
@@ -16,5 +19,7 @@ public class StringCalculator {
 		return numbers == null || numbers.isEmpty();
 	}
 	
-
+	private static List<String> splitNumbers(String allNumbers) {
+		return List.of(allNumbers.split(String.format("[%s%s]", SEPARATOR, NEWLINE_SEPARATOR)));
+	}
 }
--- stringcalculator/src/test/java/org/kata/stringcalculator/StringCalculatorTest.java
+++ stringcalculator/src/test/java/org/kata/stringcalculator/StringCalculatorTest.java
@@ -6,22 +6,27 @@ import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
 	@Test
-    void assertShouldReturn0forEmptyString() {
+    void assertShouldReturn0ForEmptyString() {
         assertEquals(StringCalculator.add(""), 0);
     }
 	
 	@Test
-    void assertShouldReturn1for1AsString() {
+    void assertShouldReturn1For1AsString() {
         assertEquals(StringCalculator.add("1"), 1);
     }
 	
 	@Test
-    void assertShouldReturn3for1And2AsString() {
+    void assertShouldReturn3For1And2AsString() {
         assertEquals(StringCalculator.add("1,2"), 3);
     }
 	
 	@Test
-    void assertShouldHandleunknownAmountOfNumbers() {
+    void assertShouldHandleUnknownAmountOfNumbers() {
         assertEquals(StringCalculator.add("1,2,3,4,6,3,8,9"), 36);
+    }
+
+	@Test
+    void assertShouldReturn6For1AndNewlineCharAnd2And3() {
+        assertEquals(StringCalculator.add("1\n2,3"), 6);
     }
 }
