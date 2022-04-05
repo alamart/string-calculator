e3dd2db3ce88ad0ec0afe102484409c53b34230a
Alvin LAMARTINIERE <alvin.lamartiniere@hubvisory.com>
2022-04-05 17:41:46 +0200

    Init project

---
+++ stringcalculator/src/main/java/org/kata/stringcalculator/StringCalculator.java
@@ -0,0 +1,5 @@
+package org.kata.stringcalculator;
+
+public class StringCalculator {
+
+}
---
+++ stringcalculator/src/test/java/org/kata/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package org.kata.stringcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+    @Test
+    void assertjShouldWork() {
+        assertThat(true).isTrue();
+    }
+}
