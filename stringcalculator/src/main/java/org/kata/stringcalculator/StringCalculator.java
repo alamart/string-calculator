b8f8502e92a678d397e28fbf0ef533bb2c9d6890
Alvin LAMARTINIERE <alvin.lamartiniere@hubvisory.com>
2022-04-05 18:06:31 +0200

    [Step2]Feat: Handle unknown amount of numbers as input

--- stringcalculator/src/main/java/org/kata/stringcalculator/StringCalculator.java
+++ stringcalculator/src/main/java/org/kata/stringcalculator/StringCalculator.java
@@ -1,5 +1,20 @@
 package org.kata.stringcalculator;
 
+import java.util.List;
+
 public class StringCalculator {
+
+	public static int add(String numbers) {
+		if(isZeroSum(numbers)) {
+			return 0;
+		}
+		List<String> numbersList = List.of(numbers.split(","));
+		return numbersList.stream().map(Integer::valueOf).reduce(0, (acc, current) -> acc+current);
+	}
+	
+	private static boolean isZeroSum(String numbers) {
+		return numbers == null || numbers.isEmpty();
+	}
+	
 
 }
--- stringcalculator/src/test/java/org/kata/stringcalculator/StringCalculatorTest.java
+++ stringcalculator/src/test/java/org/kata/stringcalculator/StringCalculatorTest.java
@@ -1,12 +1,27 @@
 package org.kata.stringcalculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StringCalculatorTest {
-    @Test
-    void assertjShouldWork() {
-        assertThat(true).isTrue();
+	@Test
+    void assertShouldReturn0forEmptyString() {
+        assertEquals(StringCalculator.add(""), 0);
+    }
+	
+	@Test
+    void assertShouldReturn1for1AsString() {
+        assertEquals(StringCalculator.add("1"), 1);
+    }
+	
+	@Test
+    void assertShouldReturn3for1And2AsString() {
+        assertEquals(StringCalculator.add("1,2"), 3);
+    }
+	
+	@Test
+    void assertShouldHandleunknownAmountOfNumbers() {
+        assertEquals(StringCalculator.add("1,2,3,4,6,3,8,9"), 36);
     }
 }
